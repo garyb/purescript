4b7bc4ed43d6076a8dcab69b9ef150e73f6c3993
Gary Burgess <gary.burgess@gmail.com>
2017-04-16 17:45:57 +0100

    Annotate kinds during parsing

--- src/Language/PureScript/Parser/Kinds.hs
+++ src/Language/PureScript/Parser/Kinds.hs
@@ -5,6 +5,7 @@ module Language.PureScript.Parser.Kinds (parseKind) where
 
 import Prelude.Compat
 
+import Language.PureScript.AST.SourcePos
 import Language.PureScript.Kinds
 import Language.PureScript.Parser.Common
 import Language.PureScript.Parser.Lexer
@@ -12,18 +13,18 @@ import Language.PureScript.Parser.Lexer
 import qualified Text.Parsec as P
 import qualified Text.Parsec.Expr as P
 
-parseStar :: TokenParser Kind
+parseStar :: TokenParser (Kind SourceSpan)
 parseStar = symbol' "*" *>
   P.parserFail "The `*` symbol is no longer used for the kind of types.\n  The new equivalent is the named kind `Type`."
 
-parseBang :: TokenParser Kind
+parseBang :: TokenParser (Kind SourceSpan)
 parseBang = symbol' "!" *>
   P.parserFail "The `!` symbol is no longer used for the kind of effects.\n  The new equivalent is the named kind `Effect`, defined in `Control.Monad.Eff` in the `purescript-eff` library."
 
-parseNamedKind :: TokenParser Kind
-parseNamedKind = NamedKind <$> parseQualified kindName
+parseNamedKind :: TokenParser (Kind SourceSpan)
+parseNamedKind = withSourceSpan' NamedKind (parseQualified kindName)
 
-parseKindAtom :: TokenParser Kind
+parseKindAtom :: TokenParser (Kind SourceSpan)
 parseKindAtom =
   indented *> P.choice
     [ parseStar
@@ -35,8 +36,10 @@ parseKindAtom =
 -- |
 -- Parse a kind
 --
-parseKind :: TokenParser Kind
+parseKind :: TokenParser (Kind SourceSpan)
 parseKind = P.buildExpressionParser operators parseKindAtom P.<?> "kind"
   where
-  operators = [ [ P.Prefix (symbol' "#" >> return Row) ]
-              , [ P.Infix (rarrow >> return FunKind) P.AssocRight ] ]
+  operators =
+    [ [ P.Prefix (withSourceSpan' (\ss _ -> Row ss) (symbol' "#")) ]
+    , [ P.Infix (withSourceSpan' (\ss _ -> FunKind ss) rarrow) P.AssocRight ]
+    ]
