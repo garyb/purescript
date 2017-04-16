37eff9960fa8e6b8d6ce3bbd3fbf0cc096e2ebeb
Gary Burgess <gary.burgess@gmail.com>
2017-04-16 03:51:40 +0100

    Add annotations to Binder

--- src/Language/PureScript/AST/Binders.hs
+++ src/Language/PureScript/AST/Binders.hs
@@ -14,33 +14,33 @@ import Language.PureScript.Types
 -- |
 -- Data type for binders
 --
-data Binder
+data Binder ta va
   -- |
   -- Wildcard binder
   --
-  = NullBinder
+  = NullBinder va
   -- |
   -- A binder which matches a literal
   --
-  | LiteralBinder (Literal Binder)
+  | LiteralBinder va (Literal (Binder ta va))
   -- |
   -- A binder which binds an identifier
   --
-  | VarBinder Ident
+  | VarBinder va Ident
   -- |
   -- A binder which matches a data constructor
   --
-  | ConstructorBinder (Qualified (ProperName 'ConstructorName)) [Binder]
+  | ConstructorBinder va (Qualified (ProperName 'ConstructorName)) [Binder ta va]
   -- |
   -- A operator alias binder. During the rebracketing phase of desugaring,
   -- this data constructor will be removed.
   --
-  | OpBinder (Qualified (OpName 'ValueOpName))
+  | OpBinder va (Qualified (OpName 'ValueOpName))
   -- |
   -- Binary operator application. During the rebracketing phase of desugaring,
   -- this data constructor will be removed.
   --
-  | BinaryNoParensBinder Binder Binder Binder
+  | BinaryNoParensBinder va (Binder ta va) (Binder ta va) (Binder ta va)
   -- |
   -- Explicit parentheses. During the rebracketing phase of desugaring, this
   -- data constructor will be removed.
@@ -48,44 +48,43 @@ data Binder
   -- Note: although it seems this constructor is not used, it _is_ useful,
   -- since it prevents certain traversals from matching.
   --
-  | ParensInBinder Binder
+  | ParensInBinder va (Binder ta va)
   -- |
   -- A binder which binds its input to an identifier
   --
-  | NamedBinder Ident Binder
+  | NamedBinder va Ident (Binder ta va)
   -- |
   -- A binder with source position information
   --
-  | PositionedBinder SourceSpan [Comment] Binder
+  | PositionedBinder va SourceSpan [Comment] (Binder ta va)
   -- |
   -- A binder with a type annotation
   --
-  | TypedBinder Type Binder
+  | TypedBinder va (Type ta) (Binder ta va)
   deriving (Show, Eq, Ord)
 
 -- |
 -- Collect all names introduced in binders in an expression
 --
-binderNames :: Binder -> [Ident]
+binderNames :: Binder ta va -> [Ident]
 binderNames = go []
   where
-  go ns (LiteralBinder b) = lit ns b
-  go ns (VarBinder name) = name : ns
-  go ns (ConstructorBinder _ bs) = foldl go ns bs
-  go ns (BinaryNoParensBinder b1 b2 b3) = foldl go ns [b1, b2, b3]
-  go ns (ParensInBinder b) = go ns b
-  go ns (NamedBinder name b) = go (name : ns) b
-  go ns (PositionedBinder _ _ b) = go ns b
-  go ns (TypedBinder _ b) = go ns b
-  go ns _ = ns
-  lit ns (ObjectLiteral bs) = foldl go ns (map snd bs)
-  lit ns (ArrayLiteral bs) = foldl go ns bs
-  lit ns _ = ns
+    go ns (LiteralBinder _ b) = lit ns b
+    go ns (VarBinder _ name) = name : ns
+    go ns (ConstructorBinder _ _ bs) = foldl go ns bs
+    go ns (BinaryNoParensBinder _ b1 b2 b3) = foldl go ns [b1, b2, b3]
+    go ns (ParensInBinder _ b) = go ns b
+    go ns (NamedBinder _ name b) = go (name : ns) b
+    go ns (PositionedBinder _ _ _ b) = go ns b
+    go ns (TypedBinder _ _ b) = go ns b
+    go ns _ = ns
+    lit ns (ObjectLiteral bs) = foldl go ns (fmap snd bs)
+    lit ns (ArrayLiteral bs) = foldl go ns bs
+    lit ns _ = ns
 
-isIrrefutable :: Binder -> Bool
-isIrrefutable NullBinder = True
-isIrrefutable (VarBinder _) = True
-isIrrefutable (PositionedBinder _ _ b) = isIrrefutable b
-isIrrefutable (TypedBinder _ b) = isIrrefutable b
+isIrrefutable :: Binder ta va -> Bool
+isIrrefutable NullBinder{} = True
+isIrrefutable VarBinder{} = True
+isIrrefutable (PositionedBinder _ _ _ b) = isIrrefutable b
+isIrrefutable (TypedBinder _ _ b) = isIrrefutable b
 isIrrefutable _ = False
-
